db5ca28f672756f1f95e59315a5097c590c31de4
Breno Santos <brenosantos145@gmail.com>
2023-10-11 20:11:59 -0300

    feat: adding basic router

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/breno5g/gin-rest-api/router"
+)
 
 func main() {
+	router.Init()
 	fmt.Println("Hello, World!")
 }
---
+++ router/router.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Init() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+	r.Run(":3001")
+}
