a3b7607dc368fa7d735c1eb894808971e509266b
Breno Santos <brenosantos145@gmail.com>
2023-10-11 22:08:09 -0300

    feat: editing task

--- handler/editTask.go
+++ handler/editTask.go
@@ -3,13 +3,43 @@ package handler
 import (
 	"net/http"
 
+	"github.com/breno5g/gin-rest-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func EditTask(ctx *gin.Context) {
-	ctx.Header("content-type", "application/json")
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "success",
-		"data":    "Edit task",
-	})
+	id := ctx.Param("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, "Param id is required")
+		return
+	}
+
+	request := UpdateTaskRequest{}
+	err := ctx.BindJSON(&request)
+	if err != nil {
+		logger.Errorf("error binding request: %v", err)
+		sendError(ctx, http.StatusBadRequest, "error binding request")
+		return
+	}
+
+	var task schemas.Task
+
+	err = db.First(&task, id).Error
+	if err != nil {
+		sendError(ctx, http.StatusNotFound, "Task not found")
+		return
+	}
+
+	task.Title = request.Title
+	task.Description = request.Description
+	task.Completed = request.Completed
+
+	err = db.Save(&task).Error
+	if err != nil {
+		logger.Errorf("error updating task: %v", err)
+		sendError(ctx, http.StatusInternalServerError, "error updating task")
+		return
+	}
+
+	sendSuccess(ctx, "editTask", task)
 }
