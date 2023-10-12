1a8f0b67506afdb10a19f6ee41ceb7053f7f81f1
Breno Santos <brenosantos145@gmail.com>
2023-10-11 21:58:33 -0300

    feat: creating task

--- handler/createTask.go
+++ handler/createTask.go
@@ -3,13 +3,32 @@ package handler
 import (
 	"net/http"
 
+	"github.com/breno5g/gin-rest-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateTask(ctx *gin.Context) {
-	ctx.Header("Content-type", "application/json")
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "success",
-		"data":    "Create task",
-	})
+	request := CreateTaskRequest{}
+
+	err := ctx.ShouldBindJSON(&request)
+	if err != nil {
+		logger.Errorf("error binding request: %v", err)
+		sendError(ctx, http.StatusBadRequest, "error binding request")
+		return
+	}
+
+	newTask := schemas.Task{
+		Title:       request.Title,
+		Description: request.Description,
+		Completed:   false,
+	}
+
+	err = db.Create(&newTask).Error
+	if err != nil {
+		logger.Errorf("error creating task: %v", err)
+		sendError(ctx, http.StatusInternalServerError, "error creating task")
+		return
+	}
+
+	sendSuccess(ctx, "createTask", newTask)
 }
