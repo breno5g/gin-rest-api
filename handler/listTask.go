322b235119c5559e36f43929001a543f94a818a4
Breno Santos <brenosantos145@gmail.com>
2023-10-11 21:49:58 -0300

    fix: initialize handlers

--- handler/listTask.go
+++ handler/listTask.go
@@ -13,9 +13,9 @@ func ListTask(ctx *gin.Context) {
 	err := db.Find(&tasks).Error
 	if err != nil {
 		logger.Errorf("error getting tasks: %v", err)
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "error getting tasks"})
+		sendError(ctx, http.StatusInternalServerError, "error getting tasks")
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{"tasks": tasks})
+	sendSuccess(ctx, "listTasks", tasks)
 }
