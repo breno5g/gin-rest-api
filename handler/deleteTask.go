58bc24a7fbd0e9825bb7c8106083b0f30152bcfc
Breno Santos <brenosantos145@gmail.com>
2023-10-11 22:03:34 -0300

    feat: deleting task

--- handler/deleteTask.go
+++ handler/deleteTask.go
@@ -3,13 +3,30 @@ package handler
 import (
 	"net/http"
 
+	"github.com/breno5g/gin-rest-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteTask(ctx *gin.Context) {
-	ctx.Header("content-type", "application/json")
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "success",
-		"data":    "Delete Task",
-	})
+	id := ctx.Param("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, "Param id is required")
+		return
+	}
+
+	task := db.First(&schemas.Task{}, id).Error
+
+	if task != nil {
+		sendError(ctx, http.StatusNotFound, "Task not found")
+		return
+	}
+
+	err := db.Delete(&schemas.Task{}, id).Error
+	if err != nil {
+		logger.Errorf("error deleting task: %v", err)
+		sendError(ctx, http.StatusInternalServerError, "error deleting task")
+		return
+	}
+
+	sendSuccess(ctx, "deleteTask", nil)
 }
